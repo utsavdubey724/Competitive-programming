273ea0f46263ac3d954e8b50d7f6c15d652bcbdf
utsavdubey724 <55616400+utsavdubey724@users.noreply.github.com>
2021-01-06 21:34:49 +0530

    Create arrayRunningSum.cpp

---
+++ arrayRunningSum.cpp
@@ -0,0 +1,41 @@
+/*
+Problem:    Running Sum of 1D Array 
+Date:       Jul 12, 2013
+Author:     Utsav Dubey, utsav_201700433@smit.smu.edu.in
+Difficulty: Easy
+Source:     https://leetcode.com/problems/running-sum-of-1d-array/
+
+Question:
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+Return the running sum of nums.
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+Example 2:
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+Example 3:
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+ 
+Constraints:
+1 <= nums.length <= 1000
+-10^6 <= nums[i] <= 10^6
+ */
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            ans.push_back(ans[i-1]+nums[i]);
+        }
+        return ans;
+    }
+};
