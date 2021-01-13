2785bb89a67e33d854cf4213175e9296377c40db
utsavdubey724 <55616400+utsavdubey724@users.noreply.github.com>
2021-01-13 18:12:54 +0530

    Create reverse.cpp

---
+++ Leetcode/C++/Easy/reverse.cpp
@@ -0,0 +1,50 @@
+/*
+Problem:    Reverse Integer
+Date:       Jan 13, 2021
+Author:     Utsav Dubey, utsav_201700433@smit.smu.edu.in
+Difficulty: Easy
+Source:     https://leetcode.com/problems/reverse-integer/
+
+Question:
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the 
+signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+Example 1:
+Input: x = 123
+Output: 321
+
+Example 2:
+Input: x = -123
+Output: -321
+
+Example 3:
+Input: x = 120
+Output: 21
+
+Example 4:
+Input: x = 0
+Output: 0
+ 
+
+Constraints:
+-231 <= x <= 231 - 1
+*/
+
+
+namespace{
+class Solution {
+public:
+    int reverse(int x) {
+        long rev = 0;
+        while(x!=0){
+            rev=rev*10+x%10;
+            x/=10;
+        }
+        if(rev>INT_MAX || rev<INT_MIN)
+            return 0;
+        return rev;
+    }
+};
+    static const auto fast=[](){ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return nullptr;}();
+}
