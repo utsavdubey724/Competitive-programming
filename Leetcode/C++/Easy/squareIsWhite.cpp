aefe6bad771ba52969e45a9d5c399af9eb6f0912
utsavdubey724 <55616400+utsavdubey724@users.noreply.github.com>
2021-07-08 15:13:12 +0530

    Create squareIsWhite.cpp

---
+++ Leetcode/C++/Easy/squareIsWhite.cpp
@@ -0,0 +1,51 @@
+/*
+Problem:    1812. Determine Color of a Chessboard Square
+Date:       July 08, 2021
+Author:     Utsav Dubey, utsav_201700433@smit.smu.edu.in
+Difficulty: Easy
+Source:     https://leetcode.com/problems/determine-color-of-a-chessboard-square/
+
+Question:
+You are given coordinates, a string that represents the coordinates of a square of the chessboard. Below is a chessboard for your reference.
+
+Return true if the square is white, and false if the square is black.
+The coordinate will always represent a valid chessboard square. The coordinate will always have the letter first, and the number second.
+
+Example 1:
+Input: coordinates = "a1"
+Output: false
+Explanation: From the chessboard above, the square with coordinates "a1" is black, so return false.
+
+Example 2:
+Input: coordinates = "h3"
+Output: true
+Explanation: From the chessboard above, the square with coordinates "h3" is white, so return true.
+
+Example 3:
+Input: coordinates = "c7"
+Output: false
+ 
+Constraints:
+coordinates.length == 2
+'a' <= coordinates[0] <= 'h'
+'1' <= coordinates[1] <= '8'
+*/
+  
+class Solution {
+public:
+    bool squareIsWhite(string coordinates) {
+        if(coordinates[0] == 'a' || coordinates[0] == 'c' || coordinates[0] == 'e' || coordinates[0] == 'g'){
+            if(coordinates[1] == '2' || coordinates[1] == '4' || coordinates[1] == '6' || coordinates[1] == '8'){
+                return true;
+            }
+            return false;
+        }
+        else{
+            if(coordinates[1] == '2' || coordinates[1] == '4' || coordinates[1] == '6' || coordinates[1] == '8'){
+                return false;
+            }
+            return true;
+        }
+        
+    }
+};
