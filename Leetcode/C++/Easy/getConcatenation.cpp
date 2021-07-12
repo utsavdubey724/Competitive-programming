abaf9147f7c44a3c37ed9ead432715a48330aa2e
utsavdubey724 <55616400+utsavdubey724@users.noreply.github.com>
2021-07-12 23:33:40 +0530

    Create getConcatenation.cpp

---
+++ Leetcode/C++/Easy/getConcatenation.cpp
@@ -0,0 +1,45 @@
+/*
+Problem:    1929. Concatenation of Array
+Date:       July 12, 2021
+Author:     Utsav Dubey, utsav_201700433@smit.smu.edu.in
+Difficulty: Easy
+Source:     https://leetcode.com/problems/concatenation-of-array/
+Question:
+Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+Specifically, ans is the concatenation of two nums arrays.
+Return the array ans.
+
+Example 1:
+Input: nums = [1,2,1]
+Output: [1,2,1,1,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+- ans = [1,2,1,1,2,1]
+
+Example 2:
+Input: nums = [1,3,2,1]
+Output: [1,3,2,1,1,3,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+- ans = [1,3,2,1,1,3,2,1]
+ 
+
+Constraints:
+n == nums.length
+1 <= n <= 1000
+1 <= nums[i] <= 1000
+*/
+
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        vector<int> ans(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++)
+            ans.push_back(nums[i]);
+        return ans;
+    }
+};
+
