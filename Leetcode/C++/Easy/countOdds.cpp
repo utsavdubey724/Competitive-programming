0066cc3c2ca781a2f72322725d29ea88724718c6
utsavdubey724 <55616400+utsavdubey724@users.noreply.github.com>
2021-07-20 16:47:09 +0530

    Create countOdds.cpp

---
+++ Leetcode/C++/Easy/countOdds.cpp
@@ -0,0 +1,39 @@
+/*
+Problem:    1523. Count Odd Numbers in an Interval Range
+Date:       July 20, 2021
+Author:     Utsav Dubey, utsav_201700433@smit.smu.edu.in
+Difficulty: Easy
+Source:     https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+
+Question:
+Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+
+Example 1:
+Input: low = 3, high = 7
+Output: 3
+Explanation: The odd numbers between 3 and 7 are [3,5,7].
+
+Example 2:
+Input: low = 8, high = 10
+Output: 1
+Explanation: The odd numbers between 8 and 10 are [9].
+ 
+
+Constraints:
+0 <= low <= high <= 10^9
+*/
+
+
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int ans;
+        if(low%2!=0 && high%2!=0)
+            ans= (high-low)/2+1;
+        else if(low%2==0 && high%2==0)
+            ans=(high-low)/2;
+        else
+            ans=(high-low)/2+1;
+        return ans;
+    }
+};
