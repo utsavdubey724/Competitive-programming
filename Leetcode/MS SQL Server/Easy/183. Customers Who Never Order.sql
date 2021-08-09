ef0400ef2fa538892ef8179aa70c95029f63b1af
utsavdubey724 <55616400+utsavdubey724@users.noreply.github.com>
2021-08-09 15:51:57 +0530

    Update 183. Customers Who Never Order.sql

--- Leetcode/MS SQL Server/Easy/183. Customers Who Never Order.sql
+++ Leetcode/MS SQL Server/Easy/183. Customers Who Never Order.sql
@@ -36,15 +36,17 @@ Using the above tables as example, return the following:
 | Henry     |
 | Max       |
 +-----------+
+*/
 
-
-Solution:
+--Solution:
 
 SELECT c.Name AS 'Customers'
 FROM Customers c LEFT JOIN Orders o ON c.Id=o.CustomerId
 WHERE o.CustomerId IS NULL
 
 --Using Subquery
+/*
 SELECT Name AS 'Customers'
 FROM Customers 
 WHERE Id NOT IN (SELECT CustomerId FROM Orders)
+*/
