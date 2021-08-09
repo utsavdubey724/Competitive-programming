afa09c0ee0e22507baf931e8594122adb7370447
utsavdubey724 <55616400+utsavdubey724@users.noreply.github.com>
2021-08-09 23:45:39 +0530

    Create isPrefixString.cpp

---
+++ Leetcode/C++/Easy/isPrefixString.cpp
@@ -0,0 +1,51 @@
+/*
+Problem:    1961. Check If String Is a Prefix of Array
+Date:       August 09, 2021
+Author:     Utsav Dubey, utsav_201700433@smit.smu.edu.in
+Difficulty: Easy
+Source:     https://leetcode.com/problems/check-if-string-is-a-prefix-of-array/
+
+Question:
+Given a string s and an array of strings words, determine whether s is a prefix string of words.
+A string s is a prefix string of words if s can be made by concatenating the first k strings in words for some positive k no larger than words.length.
+Return true if s is a prefix string of words, or false otherwise.
+
+ 
+Example 1:
+Input: s = "iloveleetcode", words = ["i","love","leetcode","apples"]
+Output: true
+Explanation:
+s can be made by concatenating "i", "love", and "leetcode" together.
+
+Example 2:
+Input: s = "iloveleetcode", words = ["apples","i","love","leetcode"]
+Output: false
+Explanation:
+It is impossible to make s using a prefix of arr.
+ 
+
+Constraints:
+1 <= words.length <= 100
+1 <= words[i].length <= 20
+1 <= s.length <= 1000
+words[i] and s consist of only lowercase English letters.
+*/
+
+
+class Solution {
+public:
+    bool isPrefixString(string s, vector<string>& words) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
+        string current;
+        int n=words.size();
+        for(int i=0;i<n;i++){
+            current+=words[i];
+            if(current==s)
+                return true;
+        }
+        return false;
+    }
+};
