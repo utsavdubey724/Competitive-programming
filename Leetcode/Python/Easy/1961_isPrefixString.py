992eb3823e59b95a42a4c80e12aed14b0e618e96
utsavdubey724 <55616400+utsavdubey724@users.noreply.github.com>
2021-08-21 12:01:19 +0530

    Create 1961_isPrefixString.py

---
+++ Leetcode/Python/Easy/1961_isPrefixString.py
@@ -0,0 +1,47 @@
+"""
+Problem:    1961. Check If String Is a Prefix of Array
+Date:       August 21, 2021
+Author:     Utsav Dubey, utsavdubey.724@gmail.com
+Difficulty: Easy
+Source:     https://leetcode.com/problems/check-if-string-is-a-prefix-of-array/
+
+Question:
+Given a string s and an array of strings words, determine whether s is a prefix string of words.
+
+A string s is a prefix string of words if s can be made by concatenating the first k strings in words for some positive k no larger than words.length.
+
+Return true if s is a prefix string of words, or false otherwise.
+
+ 
+Example 1:
+Input: s = "iloveleetcode", words = ["i","love","leetcode","apples"]
+Output: true
+Explanation:
+s can be made by concatenating "i", "love", and "leetcode" together.
+
+Example 2:
+Input: s = "iloveleetcode", words = ["apples","i","love","leetcode"]
+Output: false
+Explanation:
+It is impossible to make s using a prefix of arr.
+ 
+
+Constraints:
+1 <= words.length <= 100
+1 <= words[i].length <= 20
+1 <= s.length <= 1000
+words[i] and s consist of only lowercase English letters.
+"""
+
+
+class Solution:
+    def isPrefixString(self, s: str, words: List[str]) -> bool:
+        n = len(words)
+        prefix = ""
+        for i in range(n):
+            prefix += words[i]
+            if(s == prefix):
+                return True
+            if(len(s) < len(prefix)):
+                break
+        return False
